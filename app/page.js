c14f697097104030032a7d360036d941aa0fc16b
brainpit <dangabarin2020@gmail.com>
2024-10-21 15:49:16 +0100

    initial commit

--- app/page.js
+++ app/page.js
@@ -28,7 +28,7 @@ export default function Home() {
   return (
    <div className="relativ">
     <AnimatePresence mode="wait">
-    {/* {isLoading &&< Preloader/>} */}
+    {isLoading &&< Preloader/>}
 
     </AnimatePresence>
     <SmoothScroll>
